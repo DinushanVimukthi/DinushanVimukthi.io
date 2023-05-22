5371bca64e06b6e6fb137e56750bcb05f09e7f62
Dinushan <2020cs202@stu.ucsc.cmb.ac.lk>
2023-05-23 01:49:13 +0530

    Created

---
+++ pages/work.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class='neon-screen'>
+      <div class='neon-text'>
+        <h1>
+          Available Soon !
+        </h1>
+      </div>
+    </div>
+  </div>
+</template>
